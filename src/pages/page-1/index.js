9d4ec5fdc505f86f22c74fd37a8f7ed420a2b08b
Alexis Recalde <alexis.recalde@hotmail.com>
2023-06-25 18:56:55 -0300

    first commit

--- src/pages/page-1/index.js
+++ src/pages/page-1/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useQuery } from "react-query";
 import { getDataPending, getDataToDeliver, getDataDelivered } from "./service";
 import { Col, Row, Spin } from "antd";
